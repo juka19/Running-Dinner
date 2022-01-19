3789bdd8c90504eb94ee7e3e15c03650518a1505
Julian <julian.kath@posteo.de>
2022-01-19 18:11:02 +0100

    Update matching.py

--- matching.py
+++ matching.py
@@ -23,10 +23,59 @@ while len(hosts) > 0:
     hosts.remove(team[1])
     df['Team'].loc[df.Name.isin(team)]=f'team#{count}'
     count+=1
+
+df['course']=''
+courses=['starters', 'main', 'dessert']
+teams=list(set(df['Team']))
+for course in courses:
+    dish=random.sample(teams, int(len(df)/2/len(courses)))
+    [teams.remove(team) for team in dish]
+    df['course'].loc[df.Team.isin(dish)]=f'{course}'
+
+starter_guests_main=list(set(df['Team'][df['course']=='main']))
+starter_guests_dessert=list(set(df['Team'][df['course']=='dessert']))
+df['starters_id']=''
+count=1
+for hoster in list(set(df['Team'][df['course']=='starters'])):
+    guests=(random.sample(starter_guests_main, 1)[0], 
+    random.sample(starter_guests_dessert, 1)[0], 
+    hoster)
+    starter_guests_main.remove(guests[0])
+    starter_guests_dessert.remove(guests[1])
+    df['starters_id'].loc[df.Team.isin(guests)]=f'starter#{count}'
+    count+=1
     
-# assign course
- # draw random sample of a third of total teams for each course 
- # assign the course to the team
+main_guests_starter=list(set(df['Team'][df['course']=='starters']))
+main_guests_dessert=list(set(df['Team'][df['course']=='dessert']))
+df['main_id']=''
+count=1
+for hoster in list(set(df['Team'][df['course']=='main'])):
+    hoster_id=df['starters_id'][df['Team']==hoster].iloc[0]
+    fmgs=[guest for guest in list(set(df['Team'][df['starters_id']!=hoster_id].loc[df.Team.isin(main_guests_starter)]))]
+    fmgd=[guest for guest in list(set(df['Team'][df['starters_id']!=hoster_id].loc[df.Team.isin(main_guests_dessert)]))]
+    guests=(fmgs.pop(0), fmgd.pop(0), hoster)
+    main_guests_starter.remove(guests[0])
+    main_guests_dessert.remove(guests[1])
+    df['main_id'].loc[df.Team.isin(guests)]=f'main#{count}'
+    count+=1
+    
+dessert_guests_starter=list(set(df['Team'][df['course']=='starters']))
+dessert_guests_main=list(set(df['Team'][df['course']=='main']))
+df['dessert_id']=''
+count=1
+for hoster in list(set(df['Team'][df['course']=='dessert'])):
+    hoster_id1=df['starters_id'][df['Team']==hoster].iloc[0]
+    hoster_id2=df['main_id'][df['Team']==hoster].iloc[0]
+    fdgm=[guest for guest in (list(set(df['Team'][df['starters_id']!=hoster_id].loc[df.Team.isin(dessert_guests_main)]))&
+                              list(set(df['Team'][df['main_id']!=hoster_id].loc[df.Team.isin(dessert_guests_main)])))]
+    fdgs=[guest for guest in (list(set(df['Team'][df['starters_id']!=hoster_id].loc[df.Team.isin(dessert_guests_starter)])) &
+                              list(set(df['Team'][df['main_id']!=hoster_id].loc[df.Team.isin(dessert_guests_starter)]))]
+    guests=(fdgs.pop(0), fdgm.pop(0), hoster)
+    main_guests_starter.remove(guests[0])
+    main_guests_main.remove(guests[1])
+    df['dessert_id'].loc[df.Team.isin(guests)]=f'dessert#{count}'
+    count+=1
+    
 
 # match with other-course teams
  # filter on course 
